292ddb0d79cf0367a9ccfead0a545a98d5444708
amador_jose <amadorjose@gmail.com>
2022-09-08 17:12:10 -0300

    Ajustes de estilo en el panel.html. Resuelto: todas las vistas reciben los datos como corresponde.

--- sistema_administrativo/resto/urls.py
+++ sistema_administrativo/resto/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('productos/', views.productos),
-    path('cliente/<str:pk_cliente>/', views.cliente),
+    path('', views.home, name="home"),
+    path('productos/', views.productos, name="productos"),
+    path('cliente/<str:pk_cliente>/', views.cliente, name="cliente"),
     path('crear_pedido/', views.crearPedido, name="crear_pedido"),
 ]
