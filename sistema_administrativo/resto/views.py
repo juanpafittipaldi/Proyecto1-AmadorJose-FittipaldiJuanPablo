78efe41ae8bd53f74fc97aa8bfa42d28d582ff2f
amador_jose <amadorjose@gmail.com>
2022-09-07 21:34:19 -0300

    Resuelto: ya aparecen rendeados los productos en el template productos.html

--- sistema_administrativo/resto/views.py
+++ sistema_administrativo/resto/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import * 
 # Create your views here.
 def home(request):
     return render(request, 'resto/panel.html')
 
 def productos(request):
-    return render(request, 'resto/productos.html')
+    productos = Producto.objects.all()
+    return render(request, 'resto/productos.html', {'lista' :productos})
 
 def cliente(request):
     return render(request, 'resto/cliente.html')    
